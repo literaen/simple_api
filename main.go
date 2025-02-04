cc6e53a7edbc722aa24227d30609c2c452d8cf86
literaen <literaen@gmail.com>
2025-02-04 21:13:19 +0300

    Added task POST, GET handler

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World")
+var task string
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+func taskHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		fmt.Fprintf(w, "Hello %s", task)
+	} else if r.Method == http.MethodPost {
+		dec := json.NewDecoder(r.Body)
+
+		var m requestBody
+		if err := dec.Decode(&m); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		task = m.Message
+		fmt.Fprint(w, "success")
+	}
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/task", taskHandler).Methods("GET", "POST")
 
 	http.ListenAndServe("localhost:8080", router)
 }
