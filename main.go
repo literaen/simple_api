482b2a36b3bf177011efa278482a73b6c0a5a4c6
literaen <literaen@gmail.com>
2025-02-05 02:42:10 +0300

    Added Update, Delete task methods

--- main.go
+++ main.go
@@ -29,8 +29,49 @@ func CreateTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	DB.Create(&task)
+	result := DB.Create(&task)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	fmt.Fprint(w, "Задание успешно добавлено")
+}
+
+func UpdateTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	dec := json.NewDecoder(r.Body)
+
+	if err := dec.Decode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	result := DB.Save(&task)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Задание %d успешно обновлено", task.ID)
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	dec := json.NewDecoder(r.Body)
+
+	if err := dec.Decode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	result := DB.Delete(&task)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Задание %d успешно удалено", task.ID)
 }
 
 func main() {
@@ -41,6 +82,8 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/tasks", GetTasks).Methods("GET")
 	router.HandleFunc("/api/tasks", CreateTask).Methods("POST")
+	router.HandleFunc("/api/tasks", UpdateTask).Methods("PATCH")
+	router.HandleFunc("/api/tasks", DeleteTask).Methods("DELETE")
 
 	http.ListenAndServe("localhost:8080", router)
 }
