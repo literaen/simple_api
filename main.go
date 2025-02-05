68fb3ee8ba9aced17f3250b1ad056f5e5bfd09e1
literaen <literaen@gmail.com>
2025-02-05 18:33:05 +0300

    Added JSON response

--- main.go
+++ main.go
@@ -4,21 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
-
-func GetTasks(w http.ResponseWriter, r *http.Request) {
-	var task []Task
-	tasks := DB.Find(&task)
-
-	if tasks.Error != nil {
-		http.Error(w, tasks.Error.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprint(w, task)
-}
 
 func CreateTask(w http.ResponseWriter, r *http.Request) {
 	var task Task
@@ -35,10 +24,38 @@ func CreateTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprint(w, "Задание успешно добавлено")
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+	var task []Task
+	tasks := DB.Find(&task)
+
+	if tasks.Error != nil {
+		http.Error(w, tasks.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func UpdateTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	var task Task
 	dec := json.NewDecoder(r.Body)
 
@@ -47,31 +64,49 @@ func UpdateTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	result := DB.Save(&task)
+	updatedTask := Task{
+		Task:   task.Task,
+		IsDone: task.IsDone,
+	}
+
+	result := DB.Model(&Task{}).Where("id = ?", taskID).Updates(updatedTask)
 	if result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Fprintf(w, "Задание %d успешно обновлено", task.ID)
+	var newTask Task
+	if err := DB.First(&newTask, taskID).Error; err != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(&newTask); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Printf("Обновляем задачу с ID: %d. Новые данные: Task=%s, IsDone=%v\n", taskID, updatedTask.Task, updatedTask.IsDone)
 }
 
 func DeleteTask(w http.ResponseWriter, r *http.Request) {
-	var task Task
-	dec := json.NewDecoder(r.Body)
-
-	if err := dec.Decode(&task); err != nil {
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	result := DB.Delete(&task)
+	var task Task
+
+	result := DB.Where("id = ?", taskID).Delete(&task)
 	if result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Fprintf(w, "Задание %d успешно удалено", task.ID)
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func main() {
@@ -80,10 +115,10 @@ func main() {
 	DB.AutoMigrate(&Task{})
 
 	router := mux.NewRouter()
-	router.HandleFunc("/api/tasks", GetTasks).Methods("GET")
 	router.HandleFunc("/api/tasks", CreateTask).Methods("POST")
-	router.HandleFunc("/api/tasks", UpdateTask).Methods("PATCH")
-	router.HandleFunc("/api/tasks", DeleteTask).Methods("DELETE")
+	router.HandleFunc("/api/tasks", GetTasks).Methods("GET")
+	router.HandleFunc("/api/tasks/{id}", UpdateTask).Methods("PATCH")
+	router.HandleFunc("/api/tasks/{id}", DeleteTask).Methods("DELETE")
 
 	http.ListenAndServe("localhost:8080", router)
 }
