fde633c45f4ec0686c894acfefbdd1ca2742120c
literaen <literaen@gmail.com>
2025-02-09 12:48:13 +0300

    Added linter

--- internal/handlers/taskHandlers.go
+++ internal/handlers/taskHandlers.go
@@ -2,13 +2,8 @@ package handlers
 
 import (
 	"context"
-	"encoding/json"
-	"net/http"
 	"simple_api/internal/taskService"
 	"simple_api/internal/web/tasks"
-	"strconv"
-
-	"github.com/gorilla/mux"
 )
 
 type Handler struct {
@@ -115,82 +110,4 @@ func NewHandler(service *taskService.TaskService) *Handler {
 	return &Handler{
 		Service: service,
 	}
-}
-
-func (h *Handler) GetTaskHandler(w http.ResponseWriter, r *http.Request) {
-	tasks, err := h.Service.GetAllTasks()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tasks)
-}
-
-func (h *Handler) PostTaskHandler(w http.ResponseWriter, r *http.Request) {
-	var task taskService.Task
-
-	dec := json.NewDecoder(r.Body)
-	if err := dec.Decode(&task); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	newTask, err := h.Service.CreateTask(task)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(&newTask)
-}
-
-func (h *Handler) PatchTaskHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	taskID, err := strconv.ParseUint(vars["id"], 10, 0)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var task taskService.Task
-	dec := json.NewDecoder(r.Body)
-
-	if err := dec.Decode(&task); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	updatedTask := taskService.Task{
-		Task:   task.Task,
-		IsDone: task.IsDone,
-	}
-
-	updatedTask, err = h.Service.UpdateTaskByID(uint(taskID), updatedTask)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(&updatedTask)
-}
-
-func (h *Handler) DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	taskID, err := strconv.ParseUint(vars["id"], 10, 0)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = h.Service.DeleteTaskByID(uint(taskID))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusNoContent)
 }
